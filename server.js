751a1529a619e93177ae93854bedfb0ec54be17a
vframbach <vframbach@gmail.com>
2015-11-15 11:58:58 -0800

    add update and delete to server

--- server.js
+++ server.js
@@ -29,18 +29,71 @@ app.get('/', function (req, res) {
 // API Routes
 
 
-// test data
-// var allPosts  = [
-//     	{ post: 'Post #1', description: 'description' },
-//     	{ post: 'Post #2', description: 'description' }
-//     ];
-
 app.get('/api/posts', function (req, res) {
 	Post.find(function (err, allPosts) {
 		res.json({ posts: allPosts });
 	});
 });
 
+// get one post
+app.get('/api/posts/:id', function (req, res) {
+	// get post ID from url params and save to variable
+	var postId = parseInt(req.params.id);
+	//find post we want to read
+	var foundPost = posts.filter(function(post) {
+		return post._id == postId;
+	})[0];
+
+	// send foundPost as JSON response
+	res.json(foundPost);
+});
+
+
+// create new post
+app.post('/api/posts', function (req, res) {
+	var newPost = req.body;
+
+	if (posts.length > 0) {
+		newPost._id = posts[posts.length - 1]._id + 1;
+	} else {
+		newPost._id = 1;
+	}
+
+	posts.push(newPost);
+
+	res.json(newPost);
+});
+
+// update post
+
+app.put('/api/posts/:id', function(req, res) {
+	// get post ID from url params and save to variable
+	var postId = parseInt(req.params.id);
+	// use ID to find post to update
+	var postToUpdate = posts.filter(function (post) {
+		return post._id == postId;
+	})[0];
+
+	postToUpdate.post = req.body.post;
+	postToUpdate.description = req.body.description;
+	// respond with updated post
+	res.json(postToUpdate);
+});
+
+// delete post
+app.delete('/api/posts/:id', function(req, res) {
+	// get post ID from url params and save to variable
+	var postId = parseInd(req.params.id);
+	//use ID to find post to delete
+	var postToDelete = posts.filter(function(post) {
+		return post._id ==postId;
+	})[0];
+	//remove post from database
+	posts.splice(posts.indexOf(postToDelete), 1);
+
+	//respond with deleted post
+	res.json(postToDelete);
+});
 
 // starts server on localhost
 app.listen(process.env.PORT || 3000, function() {
